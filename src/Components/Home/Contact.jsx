d06f231fd414eb0ee93a17639795a6e24a32e896
Andreas Gunnahr <Andreas.Gunnahr@hotmail.com>
2020-03-05 22:39:10 +0100

    added responsiveness to contact section

--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -18,39 +18,45 @@ const Contact = () => {
   return (
     <section id="contact" className="contact">
       <h1 className="contact-title">
-        Kontakta oss idag för<br></br>
-        en fri <span className="underline">konsultation</span>
+        Kontakta oss <br></br>
+        direkt <span className="underline">idag</span>
       </h1>
       <div className="green-container">
-        <h1 className="second-title">
-          Tveka inte att höra av er om ni har frågor eller fundering!
-        </h1>
-        <p className="location">
-          <FontAwesomeIcon icon={faMapMarkerAlt} />
-          Storgatan 61 732 45, Arboga
-        </p>
-        <p className="phone">
-          <FontAwesomeIcon icon={faPhoneAlt} />
-          073-399 53 93
-        </p>
-        <p className="email">
-          <FontAwesomeIcon icon={faEnvelope} />
-          Alfons@aftraningorehab.se
-        </p>
-        <h1 className="social-title">Följ oss på sociala medier</h1>
-        <div className="social-container">
-          <a href="/" className="social-a">
-            <FontAwesomeIcon icon={faTwitter} />
-            Twitter
-          </a>
-          <a href="/" className="social-a">
-            <FontAwesomeIcon icon={faInstagram} />
-            Instagram
-          </a>
-          <a href="/" className="social-a">
-            <FontAwesomeIcon icon={faFacebookF} />
-            Facebook
-          </a>
+        <div className="inner-wrapper">
+          <div className="info-container">
+            <h1 className="second-title">
+              Välkommen att kontakta oss för en kostnadsfri konsultation!
+            </h1>
+            <p className="location">
+              <FontAwesomeIcon icon={faMapMarkerAlt} />
+              Storgatan 61 732 45, Arboga
+            </p>
+            <p className="phone">
+              <FontAwesomeIcon icon={faPhoneAlt} />
+              073-399 53 93
+            </p>
+            <p className="email">
+              <FontAwesomeIcon icon={faEnvelope} />
+              Alfons@aftraningorehab.se
+            </p>
+          </div>
+          <div className="social-outer">
+            <h1 className="social-title">Följ oss på sociala medier</h1>
+            <div className="social-container">
+              <a href="/" className="social-a">
+                <FontAwesomeIcon icon={faTwitter} />
+                Twitter
+              </a>
+              <a href="/" className="social-a">
+                <FontAwesomeIcon icon={faInstagram} />
+                Instagram
+              </a>
+              <a href="/" className="social-a">
+                <FontAwesomeIcon icon={faFacebookF} />
+                Facebook
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <ContactForm />
