5679394c1b4025a2c8fdd7893d3c3ff2db007690
Andreas <Andreas.Gunnahr@hotmail.com>
2020-04-26 16:27:04 +0200

    deleted some files and fixed pathing to video

--- src/Components/Blog/Blog.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "Styles/Blog/blog.scss";
-
-const Blog = () => {
-  return <div>Blog</div>;
-};
-
-export default Blog;
--- src/Components/Contact/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "Styles/Contact/contact.scss";
-
-const Contact = () => {
-  return <div>Contact</div>;
-};
-
-export default Contact;
